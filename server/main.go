83ad97c75c7919bc0dc22346f2583b85e06b6287
Cody Christ <cr999c999@gmail.com>
2024-04-18 22:08:07 -0700

    Initialized Golang backend with PostgreSQL connection

---
+++ server/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	// Load environment variables from .env file
+	err := godotenv.Load() // This will load .env file
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+
+	// Retrieve environment variables
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbSSLMode := os.Getenv("DB_SSLMODE")
+
+	// Create connection string
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
+		dbUser, dbPassword, dbHost, dbPort, dbName, dbSSLMode)
+
+	// Connect to the database
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Start the server
+	http.ListenAndServe(":8080", r)
+}
+
+// HomeHandler handles the home route
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
+}
