474744a1528cdabb5d8760860bc804bdbe924ca8
Cody Christ <cr999c999@gmail.com>
2024-04-19 16:01:36 -0700

    Connected frontend with backend to display dynamic vulnerabilities data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -21,26 +21,8 @@ function App() {
   const onSearch = async () => {
     try {
       setLoading(true);
-      // const { data } = await axios.get(`${Environment.SERVER_BASE_URL}/api/vulnerabilities`);
-      // setVulnerabilities(data);
-      setVulnerabilities([
-        {
-          id: 0,
-          cve_id: '2024-0001',
-          published: new Date(),
-          status: 'Analyzed',
-          description: 'An information exposure vulnerability in the Palo Alto Networks Cortex XDR agent on Windows devices allows a local system administrator to disclose the admin password for the agent in cleartext, which bad actors can then use to execute privileged cytool commands that disable or uninstall the agent.',
-          cvss_score: 9.1
-        },
-        {
-          id: 1,
-          cve_id: '2024-0001',
-          published: new Date(),
-          status: 'Analyzed',
-          description: 'An information exposure vulnerability in the Palo Alto Networks Cortex XDR agent on Windows devices allows a local system administrator to disclose the admin password for the agent in cleartext, which bad actors can then use to execute privileged cytool commands that disable or uninstall the agent.',
-          cvss_score: 9.1
-        },
-      ])
+      const { data } = await axios.get(`${Environment.SERVER_BASE_URL}/api/vulnerabilities`);
+      setVulnerabilities(data.slice(0, 100));
     } catch (err) {
       console.log("Error while fetching data: ", err);
       alert("Something went wrong.");
@@ -50,9 +32,9 @@ function App() {
   };
 
   return (
-    <div className='App'>
+    <div className='App max-sm:px-0'>
       <div className='flex flex-col justify-center h-full'>
-        <main className='min-w-[400px] md:min-w-[600px] lg:min-w-[800px] min-h-[600px] border rounded-xl border-solid bg-gray-900 m-auto p-4'>
+        <main className='min-w-[400px] max-w-full md:min-w-[600px] lg:min-w-[800px] min-h-[600px] border rounded-xl border-solid bg-gray-900 m-auto p-4 overflow-auto'>
           <div className='flex justify-center'>
             <button
               className='w-[200px] bg-green-500 hover:bg-green-700 disabled:bg-green-200 h-10 rounded-md flex justify-center items-center gap-1'
@@ -63,8 +45,8 @@ function App() {
               {isLoading && <Spinner />}
             </button>
           </div>
-          <div className=''>
-            <table className="min-w-full divide-y divide-gray-300">
+          <div>
+            <table className="min-w-full max-w-full divide-y divide-gray-300">
               <thead>
                 <tr>
                   <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-200 sm:pl-0">
@@ -91,17 +73,20 @@ function App() {
                       {vul.cve_id}
                     </td>
                     <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-200">
-                      {vul.published.toDateString()}
+                      {new Date(vul.published).toDateString()}
                     </td>
-                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-200 max-w-[350px]">
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-200 max-w-[150px] sm:max-w-[350px]">
                       {vul.description}
                     </td>
                     <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-200">
                       {vul.status}
                     </td>
                     <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-200">
-                      <div style={{ scale: '0.5' }}>
+                      <div style={{ scale: '0.5' }} className='hidden lg:block'>
                         <CvssScore score={vul.cvss_score} />
+                      </div>
+                      <div className='block lg:hidden text-lg'>
+                        {vul.cvss_score}
                       </div>
                     </td>
                   </tr>
